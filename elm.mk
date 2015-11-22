555f225274faa0a02dfdffce3f71ee253814d31c
Claudio Ortolina <claudio@fullyforged.com>
2015-11-22 21:43:04 +0000

    Adds task to install goat file watcher

--- elm.mk
+++ elm.mk
@@ -16,11 +16,17 @@ else
 	WELLINGTON_URL = "https://github.com/wellington/wellington/releases/download/v${WELLINGTON_VERSION}/wt_v${WELLINGTON_VERSION}_linux_amd64.zip"
 endif
 
+ifeq ($(OS),Darwin)
+	GOAT_URL = "https://s3-ap-northeast-1.amazonaws.com/yosssi/goat/darwin_amd64/goat"
+else
+	GOAT_URL = "https://s3-ap-northeast-1.amazonaws.com/yosssi/goat/linux_amd64/goat"
+endif
+
 all: build/main.js build/main.css build/index.html
 
 install: src bin build \
 				 src/Main.elm styles/main.scss index.html \
-				 bin/devd bin/wellington
+				 bin/goat bin/devd bin/wellington
 
 server:
 	bin/devd -w build -l build/
@@ -45,6 +51,10 @@ bin/wellington:
 	curl ${WELLINGTON_URL} -L -o $@.tgz
 	tar -xzf $@.tgz -C bin/ --strip 1
 	rm $@.tgz
+
+bin/goat:
+	curl ${GOAT_URL} -L -o $@
+	chmod +x $@
 
 build/main.css: styles/main.scss
 	bin/wt compile -b build/ $?
