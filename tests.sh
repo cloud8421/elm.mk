bddc852d9ffb2a8c410b5d431c7707aa6797e70b
Claudio Ortolina <claudio@fullyforged.com>
2016-03-05 16:54:12 +0000

    Adds basic tests

---
+++ tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# see https://github.com/lehmannro/assert.sh
+
+# Test is very minimal. It checks for the existence of expected files, skipping
+# those which are instrumental to the build process. For example, we can safely skip
+# testing the existence of the SASS compiler if we test the existence of the compiled
+# css file.
+
+set -e
+
+. ./assert.sh
+
+function fileExists() {
+  ls $1
+}
+
+files="bin/modd elm-package.json modd.conf \
+       index.html styles/main.scss src/interop.js src/Main.elm \
+       build/index.html build/interop.js build/main.js build/main.css"
+
+for file in $files
+do
+  assert_raises "ls dummy/$file" 0
+done
+
+assert_end examples
