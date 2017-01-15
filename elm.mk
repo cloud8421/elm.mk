2eb4d0bfb0c727a066fd6cf6bda0351dec9b4fb5
Claudio Ortolina <claudio@fullyforged.com>
2017-01-15 12:52:22 +0000

    Better help output

--- elm.mk
+++ elm.mk
@@ -54,6 +54,27 @@ else
 	MODD_URL = "https://github.com/cortesi/modd/releases/download/v${MODD_VERSION}/modd-${MODD_VERSION}-linux64.tgz"
 endif
 
+#COLORS
+GREEN  := $(shell tput -Txterm setaf 2)
+WHITE  := $(shell tput -Txterm setaf 7)
+YELLOW := $(shell tput -Txterm setaf 3)
+RESET  := $(shell tput -Txterm sgr0)
+
+# Add the following 'help' target to your Makefile
+# And add help text after each target name starting with '\#\#'
+# A category can be added with @category
+HELP_FUN = \
+					 %help; \
+					 while(<>) { push @{$$help{$$2 // 'options'}}, [$$1, $$3] if /^([a-zA-Z\-]+)\s*:.*\#\#(?:@([a-zA-Z\-]+))?\s(.*)$$/ }; \
+					 print "usage: make [target]\n\n"; \
+					 for (sort keys %help) { \
+					 print "${WHITE}$$_:${RESET}\n"; \
+					 for (@{$$help{$$_}}) { \
+					 $$sep = " " x (32 - length $$_->[0]); \
+					 print "  ${YELLOW}$$_->[0]${RESET}$$sep${GREEN}$$_->[1]${RESET}\n"; \
+					 }; \
+					 print "\n"; }
+
 all: $(COMPILE_TARGETS) ## Compiles project files
 
 install: $(INSTALL_TARGETS) ## Installs prerequisites and generates file/folder structure
@@ -72,9 +93,8 @@ test: $(TEST_TARGETS) ## Runs unit tests via elm-test
 
 prod: $(DIST_TARGETS) ## Minifies build folders for production usage
 
-help: ## Prints a help guide
-	@echo "Available tasks:"
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+help: ##@other Show this help.
+	@perl -e '$(HELP_FUN)' $(MAKEFILE_LIST)
 
 $(BUILD_FOLDER) $(BUILD_FOLDER)/images $(DIST_FOLDER) $(DIST_FOLDER)/images bin src styles images:
 	mkdir -p $@
