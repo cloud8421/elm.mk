71bda78667f6553934c5b2a311f1d5d9793990c3
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-03-05 21:51:54 +0000

    Groups targets

--- roots.mk
+++ roots.mk
@@ -7,12 +7,16 @@ NPM_BIN = ./node_modules/.bin
 ELM_VERSION = 0.18
 ELM = $(NPM_BIN)/elm
 
+# MAIN TARGETS
+
 all: $(ELM)
 .PHONY: all
 
 repl: $(ELM)
 	$(ELM) repl
 .PHONY: repl
+
+# SUPPORT TARGETS
 
 $(ELM):
 	npm install --no-save elm@${ELM_VERSION}
