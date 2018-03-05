083bf513fce1d20206842ecfc1b9a04dc69c12e6
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-03-05 22:01:53 +0000

    Adds auto-generated help

--- roots.mk
+++ roots.mk
@@ -2,6 +2,27 @@
 
 NPM_BIN := ./node_modules/.bin
 
+# COLORS
+
+GREEN  := $(shell tput -Txterm setaf 2)
+WHITE  := $(shell tput -Txterm setaf 7)
+YELLOW := $(shell tput -Txterm setaf 3)
+RESET  := $(shell tput -Txterm sgr0)
+
+# SUPPORT
+
+HELP_FUN = \
+					 %help; \
+					 while(<>) { push @{$$help{$$2 // 'options'}}, [$$1, $$3] if /^([a-zA-Z\-]+)\s*:.*\#\#(?:@([a-zA-Z\-]+))?\s(.*)$$/ }; \
+					 print "usage: make [target]\n\n"; \
+					 for (sort keys %help) { \
+					 print "${WHITE}$$_:${RESET}\n"; \
+					 for (@{$$help{$$_}}) { \
+					 $$sep = " " x (32 - length $$_->[0]); \
+					 print "  ${YELLOW}$$_->[0]${RESET}$$sep${GREEN}$$_->[1]${RESET}\n"; \
+					 }; \
+					 print "\n"; }
+
 # ELM COMPILER
 
 ELM_VERSION := 0.18
@@ -9,10 +30,14 @@ ELM := $(NPM_BIN)/elm
 
 # MAIN TARGETS
 
-all: $(ELM)
+all: $(ELM) ##@main Compiles entire project
 .PHONY: all
 
-repl: $(ELM)
+help: ##@other Displays this help text
+	@perl -e '$(HELP_FUN)' $(MAKEFILE_LIST)
+.PHONY: help
+
+repl: $(ELM) ##@main Opens an Elm repl session
 	$(ELM) repl
 .PHONY: repl
 
